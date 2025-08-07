d4e59902e7a2c1166a699099dea0cbe1ee44ed35
fezleep <virginio.9001@gmail.com>
2025-08-07 16:00:49 -0300

    Primeiro commit do frontend Angular

---
+++ src/app/dispositivos.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Dispositivos {
+  
+}
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000/api'
+};
